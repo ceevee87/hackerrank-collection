bb85d269e0b5d714792e1d56e9222137e950698c
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-09-18 15:04:04 -0600

    all tests passing on Hacker Rank.

---
+++ ProblemSolutions/BalancedBrackets.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCollection.ProblemSolutions
+{
+    class BracketStack
+    {
+        private List<char> _stack;
+        public BracketStack()
+        {
+            _stack = new List<char>();
+        }
+        public void Push(char c)
+        {
+            _stack.Insert(0, c);
+        }
+
+        public bool IsEmpty()
+        {
+            return (_stack.Count == 0);
+        }
+        public bool CanPop(char c)
+        {
+            if (_stack.Count == 0) return false;
+            char TopItemInStack = _stack.ElementAt(0);
+
+            if (c == ']' && TopItemInStack == '[') return true;
+            if (c == ')' && TopItemInStack == '(') return true;
+            if (c == '}' && TopItemInStack == '{') return true;
+            return false;
+        }
+
+        public char Pop()
+        {
+            char res = _stack.ElementAt(0);
+            _stack.RemoveAt(0);
+            return res;
+        }
+    }
+
+    public static class BalancedBrackets
+    {
+        public static string isBalanced(string s)
+        {
+            var bracketStack = new BracketStack();
+            foreach (char c in s)
+            {
+                if (c == '{' || c == '(' || c == '[')
+                {
+                    bracketStack.Push(c);
+                    continue;
+                }
+                if (c == '}' || c == ')' || c == ']')
+                {
+                    if (bracketStack.CanPop(c))
+                    {
+                        _ = bracketStack.Pop();
+                    } else
+                    {
+                        return "NO";
+                    }
+                }
+            }
+           
+            return (bracketStack.IsEmpty()) ? "YES" : "NO";
+        }
+    }
+
+}
