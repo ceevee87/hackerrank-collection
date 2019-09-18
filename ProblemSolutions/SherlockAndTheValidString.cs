ba46e5e3cdd18982082de7beb2297a641d876c42
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-09-18 09:59:27 -0600

    create unit test project create folder to house Hacker Rank problem solutions start skeleton of soln for Sherlock and the Valid String

---
+++ ProblemSolutions/SherlockAndTheValidString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCollection.ProblemSolutions
+{
+    public static class SherlockAndTheValidString
+    {
+        public static string isValid(string s)
+        {
+            return "YES";
+        }
+
+        public static void getStringToTestFromConsole()
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = isValid(s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
