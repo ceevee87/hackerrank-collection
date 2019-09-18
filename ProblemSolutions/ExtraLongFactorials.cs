83de02172f5dcff68ce6617b14d8c4cd55a64b00
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-09-18 13:24:13 -0600

    Final soln for Extra long factorials along with unit tests.

---
+++ ProblemSolutions/ExtraLongFactorials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace HackerRankCollection.ProblemSolutions
+{
+    public class ExtraLongFactorials
+    {
+        public static void extraLongFactorials(int n)
+        {
+            Console.WriteLine(CalcExtraLongFactorial(n).ToString());
+        }
+
+        public static BigInteger CalcExtraLongFactorial(int n)
+        {
+            if (n < 2) return 1;
+
+            BigInteger res = 1;
+            for (int ii = 2; ii <=n; ii++)
+            {
+                res *= ii;
+            }
+            return res;
+        }
+    }
+}
