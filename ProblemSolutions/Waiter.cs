23bfc0dd892f4da9ec54507b707a1af9f998e124
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-09-19 14:44:32 -0600

    initial creation of Waiter soln.

---
+++ ProblemSolutions/Waiter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCollection.ProblemSolutions
+{
+    public static class Waiter
+    {
+        public static List<List<int>> allResultStacks = new List<List<int>>();
+        public static List<int> aStack = new List<int>();
+        public static List<int> bStack = new List<int>();
+        public static List<int> qStack = new List<int>();
+        public static int[] aPrimes = GetPrimesUpToLimit(9739);
+
+        public static int[] GetPrimesUpToLimit(int iLastPossiblePrime)
+        {
+            bool[] aScoreboard = new bool[iLastPossiblePrime + 1];
+
+            /* Set all but 0 and 1 to prime status */
+            for (int ii = 0; ii < aScoreboard.Length; ii++) aScoreboard[ii] = true;
+            aScoreboard[0] = aScoreboard[1] = false;
+
+            /* Mark all the non-primes */
+            int iFactor = 2;
+
+            while (iFactor * iFactor <= iLastPossiblePrime)
+            {
+                /* Mark the multiples of this factor */
+                int mark = iFactor + iFactor;
+                while (mark <= iLastPossiblePrime)
+                {
+                    aScoreboard[mark] = false;
+                    mark += iFactor;
+                }
+
+                /* Set thisfactor to next prime */
+                iFactor++;
+                while (!aScoreboard[iFactor]) { iFactor++; }
+
+            }
+            /* prepare result */
+
+            List<int> result = new List<int>();
+            for (int ii = 2; ii <= iLastPossiblePrime; ii++)
+            {
+                if (aScoreboard[ii]) result.Add(ii);
+            }
+            return result.ToArray();
+        }
+
+        public static int[] waiter(int[] number, int q)
+        {
+
+            // load up the A stack from number[]
+            foreach (int v in number) aStack.Insert(0, v);
+
+            for (int iIteration = 1; iIteration <= q; iIteration++)
+            {
+                qStack.Clear();
+                foreach (int iTopPlateValue in aStack)
+                {
+                    int iPrimeNumberCheck = aPrimes[iIteration - 1];
+                    if ((iTopPlateValue % iPrimeNumberCheck) == 0)
+                    {
+                        // put the value in the B stack
+                        bStack.Insert(0, iTopPlateValue);
+                    }
+                    else
+                    {
+                        qStack.Insert(0, iTopPlateValue);
+                    }
+                }
+                aStack.Clear();
+                aStack.AddRange(qStack);
+                allResultStacks.Add(new List<int>(bStack));
+                bStack.Clear();
+            }
+            allResultStacks.Add(new List<int>(qStack));
+
+            List<int> lResult = new List<int>();
+            foreach (var l in allResultStacks)
+            {
+                foreach (var v in l)
+                {
+                    lResult.Add(v);
+                }
+            }
+            return lResult.ToArray();
+        }
+    }
+}
+
