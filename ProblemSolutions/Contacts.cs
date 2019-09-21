28ca1d2016afb9a64dd25cae9e4686b8eaf68280
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-09-21 08:44:13 -0600

    Redid the Contacts solution entirely to use a dictionary to store all possible prefixs then use that to look up contact counts. passes all hacker rank tests.

--- ProblemSolutions/Contacts.cs
+++ ProblemSolutions/Contacts.cs
@@ -8,10 +8,10 @@ namespace HackerRankCollection.ProblemSolutions
 {
     public static class Contacts
     {
-        public static int[] contacts(string[][] queries)
-        {
-            List<string> lContacts = new List<string>();
+        private static Dictionary<string, List<string>> prefixHash = new Dictionary<string, List<string>>();
 
+        private static int[] GetContactQueryCounts(string[][] queries)
+        {
             int iNumFindRequests = queries.Where(a => a[0].ToLower().Equals("find")).Count();
             int[] result = new int[iNumFindRequests];
             int iFindCounter = 0;
@@ -20,15 +20,45 @@ namespace HackerRankCollection.ProblemSolutions
             {
                 if (queries[ii][0].ToLower().Equals("add"))
                 {
-                    lContacts.Add(queries[ii][1]);
-                } else if (queries[ii][0].ToLower().Equals("find"))
+                    UpdatePreFixHash(queries[ii][1]);
+                }
+                else if (queries[ii][0].ToLower().Equals("find"))
                 {
                     var sSearchString = queries[ii][1];
-                    //var foo = lContacts.Where(c => c.StartsWith(sSearchString));
-                    result[iFindCounter++] = lContacts.Where(c => c.StartsWith(sSearchString)).Count();
+                    if (prefixHash.ContainsKey(sSearchString))
+                    {
+                        result[iFindCounter] = prefixHash[sSearchString].Count;
+                    }
+                    else
+                    {
+                        result[iFindCounter] = 0;
+                    }
+                    iFindCounter++;
                 }
             }
             return result;
         }
+
+        private static void UpdatePreFixHash(string sContactName)
+        {
+            for (int ii = 1; ii <= sContactName.Length; ii++)
+            {
+                string prefix = sContactName.Substring(0, ii);
+                if (prefixHash.ContainsKey(prefix))
+                {
+                    prefixHash[prefix].Add(sContactName);
+                }
+                else
+                {
+                    prefixHash.Add(prefix, new List<string> { sContactName });
+                }
+            }
+        }
+        public static int[] contacts(string[][] queries)
+        {
+            prefixHash.Clear();
+            return GetContactQueryCounts(queries);
+        }
+
     }
 }
