0c37ea0a2f1523c60bc55b650a9bfa60d63fc29d
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-09-29 11:06:48 -0600

    add new code to solve Find the Running Median. Skeleton code only to run a single test, read in test data, call solution. Next round of checkins will be working on the formal solution and improving test cases

---
+++ HackerRankCollectionTests/ProblemSolutionTests/FindTheRunningMedianTests.cs
@@ -0,0 +1,35 @@
+﻿using HackerRankCollection.ProblemSolutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace HackerRankCollectionTests.ProblemSolutionTests
+{
+    [TestClass]
+    public class FindTheRunningMedianTests
+    {
+        private string _sTestDataRootDir = @"..\..\TestData\FindTheRunningMedian\";
+
+        [TestMethod]
+        public void SampleTestA()
+        {
+            int[] arr = GetInputArray(_sTestDataRootDir + "sampleA_input.txt");
+            double[] result = FindTheRunningMedian.runningMedian(arr);
+            double[] expected = new double[] { 12.0, 8.0, 5.0, 4.5, 5.0, 6.0 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        private int[] GetInputArray(string inFile)
+        {
+            // zero error catching here ... make sure your input is legit
+            int[] arr;
+            using (StreamReader file = new StreamReader(inFile))
+            {
+                int n = Convert.ToInt32(file.ReadLine());
+                arr = Array.ConvertAll(file.ReadLine().Split(' '), a => Convert.ToInt32(a));
+            }
+            return arr;
+        }
+
+    }
+}
---
+++ ProblemSolutions/FindTheRunningMedian.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCollection.ProblemSolutions
+{
+    public static class FindTheRunningMedian
+    {
+        public static double[] runningMedian(int[] a)
+        {
+            return new double[] { 12.0, 8.0, 5.0, 4.5, 5.0, 6.0 };
+        }
+
+    }
+}
