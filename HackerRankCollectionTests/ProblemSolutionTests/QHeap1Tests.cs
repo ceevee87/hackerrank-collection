14a8528fd368a1683a6f10b3a54007cb65c770ac
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-10-01 21:45:29 -0600

    Downloaded two tests from hackerrank for QHeap1. Wrote file readers for QHeap1 test input and answers. Hooked up hackerrank tests 14 and 16  for QHeap1.

--- HackerRankCollectionTests/ProblemSolutionTests/QHeap1Tests.cs
+++ HackerRankCollectionTests/ProblemSolutionTests/QHeap1Tests.cs
@@ -1,18 +1,70 @@
 ﻿using HackerRankCollection.ProblemSolutions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace HackerRankCollectionTests.ProblemSolutionTests
 {
     [TestClass]
     public class QHeap1Tests
     {
+        private string _sTestDataRootDir = @"..\..\TestData\QHeap1\";
+
         [TestMethod]
-        public void Test1()
+        public void SampleATest()
         {
             string[] queries = new string[] { "1 4", "1 9", "3", "2 4", "3" };
             int[] result = QHeap1.ProcessQueries(queries);
             int[] expected = new int[] { 4, 9 };
             CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void HackerRankTest14()
+        {
+            string[] queries = GetInputData(_sTestDataRootDir + "hackerrank14_input.txt");
+            int[] expected = GetResultAnswers(_sTestDataRootDir + "hackerrank14_answer.txt");
+            int[] result = QHeap1.ProcessQueries(queries);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void HackerRankTest16()
+        {
+            string[] queries = GetInputData(_sTestDataRootDir + "hackerrank16_input.txt");
+            int[] expected = GetResultAnswers(_sTestDataRootDir + "hackerrank16_answer.txt");
+            int[] result = QHeap1.ProcessQueries(queries);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        private static string[] GetInputData(string inFile)
+        {
+            string[] queries;
+            using (StreamReader file = new StreamReader(inFile))
+            {
+                int k = Convert.ToInt32(file.ReadLine());
+                queries = new string[k];
+                for (int ii = 0; ii < k; ii++)
+                {
+                    queries[ii] = file.ReadLine();
+                }
+            }
+            return queries;
+        }
+
+        private static int[] GetResultAnswers(string inFile)
+        {
+            List<int> answers = new List<int>();
+            using (StreamReader file = new StreamReader(inFile))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    answers.Add(Convert.ToInt32(line));
+                }
+            }
+            return answers.ToArray();
         }
 
     }
