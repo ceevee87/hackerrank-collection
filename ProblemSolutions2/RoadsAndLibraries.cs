0f2fd41129100f836c197b8a3cb75385a5dd8e40
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-10-19 10:05:41 -0600

    refactored code for RoadsAndLibraries (renamed procs, removed unneccesary property on class, cleaned up main proc to only make fn calls ... no loop code or what not anymore).

--- ProblemSolutions2/RoadsAndLibraries.cs
+++ ProblemSolutions2/RoadsAndLibraries.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace HackerRankCollection.ProblemSolutions2
@@ -6,7 +7,7 @@ namespace HackerRankCollection.ProblemSolutions2
     public static class RoadsAndLibraries
     {
 
-        #region classvars
+        #region Class Prpperties
         // store which cities are connected to which cities in an
         // adjacency list. 
         private static Dictionary<int, List<City>> adjList = null;
@@ -16,10 +17,6 @@ namespace HackerRankCollection.ProblemSolutions2
         // the actual members are not important, only how many 
         // clusters we have and the size of each cluster.
         private static Dictionary<int, int> CityClusterSizes = null;
-
-        private static int cityClusterId = 1;
-
-        public static int CityClusterId { get => cityClusterId; set => cityClusterId = value; }
 
         internal class City
         {
@@ -38,26 +35,20 @@ namespace HackerRankCollection.ProblemSolutions2
 
         #endregion
 
-        private static long calcCostForRoadsAndLibraries(int numCities, int roadCost, int libraryCost)
+        private static long calcRoadAndLibraryCostForCityCluster(int numCities, int roadCost, int libraryCost)
         {
+            // if a city is isolated we must build a library for it.
             if (numCities == 1) return libraryCost;
 
+            // otherwise, there are only two possible solutions:
+            // 1. build 1 library (and never more) and have it serve a city cluster (MST)
+            // 2. build libraries for all cities
+            // it's only one of the two items above for each cluster
             if (roadCost >= libraryCost)
             {
                 return libraryCost * numCities;
             }
             return (libraryCost + roadCost * (numCities - 1));
-
-            // if Clib > Croad + (Clib - Croad)/numCities ==> do all roads + 1 library
-            // otherwise do all libraries and no road
-            //float v1 = (float)libraryCost;
-            //float v2 = (float)roadCost;
-            //float v3 = (float)(libraryCost - roadCost) / (float)numCities;
-            //if (v1 > v2 + v3)
-            //{
-            //    return (libraryCost + roadCost * (numCities - 1));
-            //}
-            //return libraryCost * numCities;
         }
 
 
@@ -72,7 +63,7 @@ namespace HackerRankCollection.ProblemSolutions2
             CityClusterSizes = new Dictionary<int, int>();
         }
 
-        private static void AddCityRoadToAdjList(int cityId1, int cityId2)
+        private static void AddCityPairToAdjList(int cityId1, int cityId2)
         {
             // should never happen because we initialzie the adjList
             // to always have an entry for each city id.
@@ -86,10 +77,16 @@ namespace HackerRankCollection.ProblemSolutions2
 
         }
 
-        private static void InitAdjList(int n)
+        private static void IntiCityAdjacenyList(int n)
         {
             if (adjList != null) adjList.Clear();
             adjList = new Dictionary<int, List<City>>();
+
+            // the adjaceny list is indexed by an integer
+            // but we need city information at each "node" of the list
+            // so, I add in the city itself as the first element in the list
+            // for each dictionary entry.
+            // example: the first entry in adjList[4] is city 4. 
             for (int ii = 0; ii < n; ii++)
             {
                 adjList.Add(ii, new List<City> { new City(ii) });
@@ -125,30 +122,34 @@ namespace HackerRankCollection.ProblemSolutions2
             return count;
         }
 
-        private static void MarkCityClusters()
+        private static void CountAndRecordCityClusterSizes()
         {
             int cityClusterId = 1;
             for (int ii = 0; ii < adjList.Count; ii++)
             {
+                // if this city is by itself (no road leaving/entering)...
                 if (adjList[ii].Count == 1)
                 {
                     if (!CityClusterSizes.ContainsKey(cityClusterId)) CityClusterSizes.Add(cityClusterId++, 1);
                     continue;
                 }
+
+                // otherwise, this city is part of a network of cities specified by 
+                // the 2D array supplied as input to this problem.
+                // find out how large that network is (cluster)
                 if (adjList[ii].ElementAt(0).CityClusterId == 0)
                 {
-                    int count = GroupCitiesIntoClusters(adjList[ii].ElementAt(0), cityClusterId);
-                    if (!CityClusterSizes.ContainsKey(cityClusterId)) CityClusterSizes.Add(cityClusterId, count);
+                    int clusterSize = GroupCitiesIntoClusters(adjList[ii].ElementAt(0), cityClusterId);
+                    if (!CityClusterSizes.ContainsKey(cityClusterId)) CityClusterSizes.Add(cityClusterId, clusterSize);
                     cityClusterId++;
                 }
             }
         }
-        // Complete the roadsAndLibraries function below.
-        public static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] cities)
+
+        private static void PopulateCityAdjacencyList(int[][] cities)
         {
-            InitAdjList(n);
-            InitCityClusterCounts();
-            CityClusterId = 1;
+            // the problem supplies city numbers starting at 1 ...
+            // I adjust these to be from 0 ...
 
             // load up the adjList
             for (int ii = 0; ii < cities.Length; ii++)
@@ -157,17 +158,30 @@ namespace HackerRankCollection.ProblemSolutions2
                 int cityId1 = cities[ii][0];
                 int cityId2 = cities[ii][1];
 
-                AddCityRoadToAdjList(cityId1 - 1, cityId2 - 1);
-                AddCityRoadToAdjList(cityId2 - 1, cityId1 - 1);
+                AddCityPairToAdjList(cityId1 - 1, cityId2 - 1);
+                AddCityPairToAdjList(cityId2 - 1, cityId1 - 1);
             }
+        }
 
-            MarkCityClusters();
+        private static long calcTotalCostForRoadsAndLibraries(int c_road, int c_lib)
+        {
             long result = 0;
             foreach (int clusterId in CityClusterSizes.Keys)
             {
-                result += calcCostForRoadsAndLibraries(CityClusterSizes[clusterId], c_road, c_lib);
+                result += calcRoadAndLibraryCostForCityCluster(CityClusterSizes[clusterId], c_road, c_lib);
             }
+            return result;
+        }
 
+        // Complete the roadsAndLibraries function below.
+        public static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] cities)
+        {
+            IntiCityAdjacenyList(n);
+            InitCityClusterCounts();
+            PopulateCityAdjacencyList(cities);
+            CountAndRecordCityClusterSizes();
+
+            long result = calcTotalCostForRoadsAndLibraries(c_road, c_lib);
             return result;
         }
     }
