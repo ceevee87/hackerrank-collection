d5ea680428e9ddcb846840a56551af9f7bec6833
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-10-26 22:46:08 -0600

    initial solution for MinimumAverageWaitTime. only three cases passing on hackerrank. wrote nunit tests for MinimumAverageWaitTime. Added in hackerrank tests 0-2, 7, and 9.

---
+++ HackerRankCollectionTests/ProblemSolutionTests2/MinimumAverageWaitTimeTests.cs
@@ -0,0 +1,101 @@
+﻿using HackerRankCollection.ProblemSolutions2;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCollectionTests.ProblemSolutionTests2
+{
+    [TestFixture]
+    public class MinimumAverageWaitTimeTests
+    {
+        string _sTestDataRootDir = string.Format(@"{0}\{1}\"
+                    , Path.GetDirectoryName(Directory.GetParent(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)).FullName)
+                    , @"TestData\MinimumAverageWaitingTime");
+
+        [Test]
+        public void SampleTest0()
+        {
+            int[][] oData = GetInputData(_sTestDataRootDir + "sample0_input.txt");
+            long result = MinimumAverageWaitTime.minimumAverage(oData);
+
+            long expected = 9;
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void HackerrankTest0()
+        {
+            int[][] oData = GetInputData(_sTestDataRootDir + "hackerrank0_input.txt");
+            long result = MinimumAverageWaitTime.minimumAverage(oData);
+
+            long expected = 1418670047;
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void HackerrankTest1()
+        {
+            int[][] oData = GetInputData(_sTestDataRootDir + "hackerrank1_input.txt");
+            long result = MinimumAverageWaitTime.minimumAverage(oData);
+
+            long expected = 8485548331;
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void HackerrankTest2()
+        {
+            int[][] oData = GetInputData(_sTestDataRootDir + "hackerrank2_input.txt");
+            long result = MinimumAverageWaitTime.minimumAverage(oData);
+
+            long expected = 6667863382;
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void HackerrankTest7()
+        {
+            int[][] oData = GetInputData(_sTestDataRootDir + "hackerrank7_input.txt");
+            long result = MinimumAverageWaitTime.minimumAverage(oData);
+
+            long expected = 8323528195457;
+            Assert.AreEqual(expected, result);
+
+        }
+
+        // performance test
+        [Test]
+        public void HackerrankTest9()
+        {
+            int[][] oData = GetInputData(_sTestDataRootDir + "hackerrank9_input.txt");
+            long result = MinimumAverageWaitTime.minimumAverage(oData);
+
+            long expected = 16631801980531;
+            Assert.AreEqual(expected, result);
+
+        }
+
+        private int[][] GetInputData(string inFile)
+        {
+            int[][] customers;
+            using (StreamReader fsr = new StreamReader(inFile))
+            {
+                int n = Convert.ToInt32(fsr.ReadLine());
+                customers = new int[n][];
+                for (int rowcnt = 0; rowcnt < n; rowcnt++)
+                {
+                    customers[rowcnt] = Array.ConvertAll(fsr.ReadLine().Split(' '), r => Convert.ToInt32(r));
+                }
+            }
+            return customers;
+        }
+    }
+}
---
+++ ProblemSolutions2/MinimumAverageWaitTime.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCollection.ProblemSolutions2
+{
+    public class Customer
+    {
+        private long _arrivalTime;
+        private long _orderLength;
+
+        public long ArrivalTime { get => _arrivalTime; set => _arrivalTime = value; }
+        public long OrderLength { get => _orderLength; set => _orderLength = value; }
+
+        public Customer(long at, long ol)
+        {
+            _arrivalTime = at;
+            _orderLength = ol;
+        }
+    }
+
+    public static class MinimumAverageWaitTime
+    {
+        /*
+         * Definitely not necessary to use dynamic 
+         * programming. Can be solved in O(n log n) 
+         * by creating and sorting a list of 
+         * the (arrival, cook_time) pairs by arrival, 
+         * then using a small min-heap, sorted by cook time, 
+         * to keep track of the people who are currently in 
+         * the pizza shop (i.e.whose arrival time < the total time elapsed) 
+         * by using the list as a stack.This is O(n log n + n log p) 
+         * time where p is the number of people in the shop 
+         * at any given time, and since p <= n, the 
+         * algorithm is O(n log n). Here is the code:
+         */
+
+        public enum HeapType { ByArrivalTime, ByOrderLength };
+
+        #region CustomerCollection
+
+        public class CustomerCollection
+        {
+            private Customer[] _heap;
+            private int _size;
+            private HeapType _orderBy;
+
+            private int Parent(int i) { return i / 2; }
+            private int Left(int i) { return 2 * i; }
+            private int Right(int i) { return ((2 * i) + 1); }
+
+
+            public int Size => _size;
+            public Customer Peek => _heap[1];
+
+            public CustomerCollection(int capacity, HeapType ht)
+            {
+                this._heap = new Customer[capacity + 1];
+                this._heap[0] = new Customer(long.MinValue, long.MinValue);
+                this._size = 0;
+                this._orderBy = ht;
+            }
+
+            private int Compare(int a, int b)
+            {
+                if (_orderBy == HeapType.ByArrivalTime)
+                {
+                    if (_heap[a].ArrivalTime < _heap[b].ArrivalTime) return -1;
+                    if (_heap[a].ArrivalTime == _heap[b].ArrivalTime) return 0;
+                    return 1;
+                }
+
+                if (_orderBy == HeapType.ByOrderLength)
+                {
+                    if (_heap[a].OrderLength < _heap[b].OrderLength) return -1;
+                    if (_heap[a].OrderLength == _heap[b].OrderLength) return 0;
+                    return 1;
+                }
+
+                // error condition
+                return Int32.MinValue;
+            }
+
+            private void Swap(int i, int j)
+            {
+                Customer tmp = _heap[i];
+                _heap[i] = _heap[j];
+                _heap[j] = tmp;
+            }
+
+            private void HeapifyDown(int i)
+            {
+                int left = Left(i);
+                int right = Right(i);
+                int smallest;
+                smallest = (left <= _size && Compare(left,i) <= 0) ? left : i;
+
+                if (right <= _size && Compare(right, smallest) < 0) smallest = right;
+
+                if (smallest != i)
+                {
+                    Swap(i, smallest);
+                    HeapifyDown(smallest);
+                }
+            }
+
+
+            private void HeapifyUp(int k)
+            {
+                while (Compare(k,Parent(k)) < 0)
+                {
+                    Swap(k, Parent(k));
+                    k = Parent(k);
+                }
+            }
+
+            public void Push(Customer x)
+            {
+                if (_size >= (_heap.Length - 1)) return;
+                _heap[++_size] = x;
+                HeapifyUp(_size);
+            }
+
+            public Customer Pop()
+            {
+                Customer head = _heap[1];
+                _heap[1] = _heap[_size--];
+                HeapifyDown(1);
+
+                return head;
+            }
+        }
+        #endregion
+
+        private static CustomerCollection InitCustomerCollection(int[][] aCustomers, HeapType heaptype)
+        {
+            CustomerCollection result = new CustomerCollection(aCustomers.Length, heaptype);
+
+            for (int ii = 0; ii < aCustomers.Length; ii++)
+            {
+                result.Push(new Customer(aCustomers[ii][0], aCustomers[ii][1]));
+            }
+            return result;
+        }
+
+        /*
+         * Complete the minimumAverage function below.
+         */
+        public static long minimumAverage(int[][] aCustomers)
+        {
+            int numCustomers = aCustomers.Length;
+
+            CustomerCollection custsByArrivalTime = InitCustomerCollection(aCustomers, HeapType.ByArrivalTime);
+            CustomerCollection debugIsh = InitCustomerCollection(aCustomers, HeapType.ByArrivalTime);
+
+            while (debugIsh.Size > 0)
+            {
+                Debug.WriteLine(debugIsh.Pop().ArrivalTime);
+            }
+
+            CustomerCollection custsByOrderLength = new CustomerCollection(aCustomers.Length, HeapType.ByOrderLength);
+            List<Customer> customerOrderQueue = new List<Customer>();
+
+            customerOrderQueue.Add(custsByArrivalTime.Pop());
+
+            long  waitTimeTotal = 0;
+            long totalTimeElapsed = customerOrderQueue.ElementAt(0).ArrivalTime;
+            while (customerOrderQueue.Count > 0)
+            {
+                Customer currentCustomer = customerOrderQueue.ElementAt(0);
+                customerOrderQueue.RemoveAt(0);
+
+                totalTimeElapsed += currentCustomer.OrderLength;
+                waitTimeTotal += (totalTimeElapsed - currentCustomer.ArrivalTime);
+
+                // get all customers who arrive while the current customer's 
+                // order is being prepared (customer arrival time + order length)
+
+                while (custsByArrivalTime.Size > 0 && custsByArrivalTime.Peek.ArrivalTime <= totalTimeElapsed)
+                {
+                    custsByOrderLength.Push(custsByArrivalTime.Pop());
+                }
+
+                // now get all the customers and order them by order length and put them into
+                // the order queue.
+                while (custsByOrderLength.Size > 0)
+                {
+                    customerOrderQueue.Add(custsByOrderLength.Pop());
+                }
+            }
+            return waitTimeTotal / numCustomers;
+        }
+    }
+}
