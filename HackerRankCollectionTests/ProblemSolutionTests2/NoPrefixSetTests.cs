67c167e2eb6beb8590b6861ed08b69dd517ac9e5
cyclopsvision87@icloud.com <cyclopsvision87@icloud.com>
2019-10-29 22:12:34 -0600

    initial solution for NoPrefixSet added in tests for NoPrefixSet.

---
+++ HackerRankCollectionTests/ProblemSolutionTests2/NoPrefixSetTests.cs
@@ -0,0 +1,72 @@
+﻿using HackerRankCollection.ProblemSolutions2;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace HackerRankCollectionTests.ProblemSolutionTests2
+{
+    [TestFixture]
+    public class NoPrefixSetTests
+    {
+        string _sTestDataRootDir = string.Format(@"{0}\{1}\"
+                    , Path.GetDirectoryName(Directory.GetParent(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)).FullName)
+                    , @"TestData\NoPrefixSet");
+
+        [Test]
+        public void hackerrank41Test()
+        {
+            string[] oData = GetInputData(_sTestDataRootDir + "hackerrank41_input.txt");
+            PrefixCheckResult result = NoPrefixSet.DoBadPrefixCheck(oData);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(true, result._result);
+                Assert.IsTrue(true);
+            });
+
+        }
+
+        [Test]
+        public void hackerrank21Test()
+        {
+            string[] oData = GetInputData(_sTestDataRootDir + "hackerrank21_input.txt");
+            PrefixCheckResult result = NoPrefixSet.DoBadPrefixCheck(oData);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(false, result._result);
+                Assert.AreEqual("aabcde", result._word);
+            });
+
+        }
+
+        [Test]
+        public void hackerrank22Test()
+        {
+            string[] oData = GetInputData(_sTestDataRootDir + "hackerrank22_input.txt");
+            PrefixCheckResult result = NoPrefixSet.DoBadPrefixCheck(oData);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(false, result._result);
+                Assert.AreEqual("aacghgh", result._word);
+            });
+
+        }
+
+        private static string[] GetInputData(string inFile)
+        {
+            string[] result;
+            using (StreamReader fsr = new StreamReader(inFile))
+            {
+                int k = Convert.ToInt32(fsr.ReadLine());
+                result = new string[k];
+                for (int ii = 0; ii < k; ii++)
+                {
+                    result[ii] = fsr.ReadLine();
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ProblemSolutions2/NoPrefixSet.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCollection.ProblemSolutions2
+{
+    public class PrefixCheckResult
+    {
+        public bool _result;
+        public string _word;
+
+        public PrefixCheckResult(bool r, string w)
+        {
+            _result = r;
+            _word = w;
+        }
+    }
+
+    public static class NoPrefixSet
+    {
+        private static Dictionary<string, byte> _prefixDict = new Dictionary<string, byte>();
+
+        private static bool IsWordSafeToAddToPrefixHash(string word)
+        {
+            if (_prefixDict.ContainsKey(word)) return false;
+
+            // check if any prefixes of the current word are in the 
+            // dictionary stored as an entire word.
+            for (int ii = 1; ii < word.Length; ii++)
+            {
+                string prefix = word.Substring(0, ii);
+                if (_prefixDict.ContainsKey(prefix) && _prefixDict[prefix] == 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void UpdatePreFixHash(string word)
+        {
+            if (!_prefixDict.ContainsKey(word))
+            {
+                _prefixDict.Add(word, 1);
+            }
+            for (int ii = 1; ii < word.Length; ii++)
+            {
+                string prefix = word.Substring(0, ii);
+                if (!_prefixDict.ContainsKey(prefix))
+                {
+                    _prefixDict.Add(prefix, 0);
+                }
+            }
+        }
+
+        private static string[] GetInputData()
+        {
+            int k = Convert.ToInt32(Console.ReadLine());
+            string[] result = new string[k];
+            for (int ii = 0; ii < k; ii++)
+            {
+                result[ii] = Console.ReadLine();
+            }
+            return result;
+        }
+
+        private static void InitPrefixMap()
+        {
+            if (_prefixDict == null)
+            {
+                _prefixDict = new Dictionary<string, byte>();
+            }
+            _prefixDict.Clear();
+        }
+
+        public static PrefixCheckResult DoBadPrefixCheck(string[] words)
+        {
+            InitPrefixMap();
+            foreach (string w in words)
+            {
+                // check if word exists in the prefix dictionary as a "prefix" type
+                if (!IsWordSafeToAddToPrefixHash(w)) return new PrefixCheckResult(false, w);
+
+                // insert all possible prefix combinations of the words into the dictionary
+                UpdatePreFixHash(w);
+            }
+            return new PrefixCheckResult(true, string.Empty);
+        }
+
+        public static void ProcessNoPrefixSet(string[] args)
+        {
+            InitPrefixMap();
+            string[] words = GetInputData();
+            PrefixCheckResult res = DoBadPrefixCheck(words);
+            if (!res._result)
+            {
+                Console.WriteLine("BAD SET");
+                Console.WriteLine(res._word);
+            }
+            else
+            {
+                Console.WriteLine("GOOD SET");
+            }
+        }
+    }
+}
